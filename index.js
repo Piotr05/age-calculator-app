036186b00b258a8ed62e460d91602cb424ed02ae
Piotr05 <czarnynoga05@gmail.com>
2023-04-26 12:12:48 +0200

    validation and calculating for improvement

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const errorMsg = document.querySelectorAll('.calculator__inputs_error-msg');
 const dayInput = document.getElementById('dayInput');
 const monthInput = document.getElementById('monthInput');
 const yearInput = document.getElementById('yearInput');
+const output = document.querySelectorAll('.calculator__output_number');
 
 // date
 const today = new Date();
@@ -19,6 +20,9 @@ document.querySelector('.calculator__button_btn').addEventListener('click', () =
         label.classList.remove('calculator__inputs_label--incorrect');
     });
     // VALIDATION
+
+    // days in month
+    const daysMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     // if empty
     if (dayInput.value === '' ||
         monthInput.value === '' ||
@@ -31,14 +35,23 @@ document.querySelector('.calculator__button_btn').addEventListener('click', () =
         if (parseInt(dayInput.value) > 31 || parseInt(dayInput.value) < 1) {
             incorrectInput()
             errorMsg[0].innerHTML = 'Must be a valid day';
+        } else if (parseInt(dayInput.value) > daysMonth[parseInt(monthInput.value) - 1]) {
+            incorrectInput()
+            errorMsg[0].innerHTML = 'Must be a valid date';
+        } else {
+            calculateDate();
         }
         if (parseInt(monthInput.value) > 12 || parseInt(monthInput.value) < 1) {
             incorrectInput();
             errorMsg[1].innerHTML = 'Must be a valid month';
+        } else {
+            calculateDate();
         }
         if (parseInt(yearInput.value) > today.getFullYear() || parseInt(yearInput.value) < 1 ) {
             incorrectInput();
             errorMsg[2].innerHTML = 'Must be in the past';
+        } else {
+            calculateDate();
         }
     }
 });
@@ -50,4 +63,19 @@ const incorrectInput = () => {
     labels.forEach(label => {
         label.classList.add('calculator__inputs_label--incorrect');
     });
+}
+
+const calculateDate = () => {
+    const date = new Date(
+        parseInt(yearInput.value),
+        parseInt(monthInput.value),
+        parseInt(dayInput.value)
+    )
+    const years = today.getFullYear() - date.getFullYear();
+    const months = today.getMonth() - date.getMonth();
+    const days = today.getDate() - date.getDate();
+
+    output[0].innerHTML = years;
+    output[1].innerHTML = months + 1;
+    output[2].innerHTML = days;
 }
